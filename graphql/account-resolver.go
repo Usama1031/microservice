38f3d54ae8baf54354fda8beb0c293ed91a4fac2
Usama1031 <usamashoukat007dz@gmail.com>
2025-07-24 13:19:49 -0700

    completing the graphql and starting the accounts

--- graphql/account-resolver.go
+++ graphql/account-resolver.go
@@ -3,3 +3,7 @@ package main
 type accountResolver struct {
 	server *Server
 }
+
+// func (r *accountResolver) Orders(ctx context.Context, obj *Account) ([]*Order, error) {
+
+// }
--- graphql/query-resolver.go
+++ graphql/query-resolver.go
@@ -3,3 +3,11 @@ package main
 type queryResolver struct {
 	server *Server
 }
+
+// func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string) ([]*Account, error) {
+
+// }
+
+// func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput, query, id *string) ([]*Product, error) {
+
+// }
