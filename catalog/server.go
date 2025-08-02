5bed2b8cf9fe68046f5d1e19e895bb36eae77984
Usama1031 <usamashoukat007dz@gmail.com>
2025-08-02 10:50:40 -0700

    catalog.proto, client and server files for catalog microservice

--- catalog/server.go
+++ catalog/server.go
@@ -1,10 +1,12 @@
 package catalog
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net"
 
-	"github.com/usama1031/go-grpc-graphql-microservice/account/pb"
+	pb "github.com/usama1031/go-grpc-graphql-microservice/account/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -28,4 +30,66 @@ func ListenGRPC(s Service, port int) error {
 
 	reflection.Register(serv)
 	return serv.Serve(lis)
+}
+
+func (s *grpcServer) PostProduct(ctx context.Context, r *pb.PostProductRequest) (*pb.PostProductResponse, error) {
+	p, err := s.service.PostProduct(ctx, r.Name, r.Description, r.Price)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &pb.PostProductResponse{Product: &pb.Product{
+		Id:          p.ID,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       p.Price,
+	}}, nil
+}
+
+func (s *grpcServer) GetProduct(ctx context.Context, r *pb.GetProductRequest) (*pb.GetProductResponse, error) {
+	p, err := s.service.GetProduct(ctx, r.Id)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return &pb.GetProductResponse{
+		Product: &pb.Product{
+			Id:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+		},
+	}, nil
+}
+
+func (s *grpcServer) GetProducts(ctx context.Context, r *pb.GetProductsRequest) (*pb.GetProductsResponse, error) {
+	var res []Product
+	var err error
+
+	if r.Query != "" {
+		res, err = s.service.SearchProducts(ctx, r.Query, r.Skip, r.Take)
+	} else if len(r.Id) != 0 {
+		res, err = s.service.GetProductsByIDs(ctx, r.Ids)
+	} else {
+		res, err = s.service.GetProducts(ctx, r.Skip, r.Take)
+	}
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	products := []*pb.Product{}
+	for _, p := range res {
+		products = append(products, &pb.Product{
+			Id:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+		})
+	}
+
+	return &pb.GetProductsResponse{Product: products}, nil
 }
