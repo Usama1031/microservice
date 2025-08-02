7655cb67e0fe2886e3fc93c16a39856b6e6d578e
Usama1031 <usamashoukat007dz@gmail.com>
2025-08-02 03:59:30 -0700

    setting the service and server for catalog microservice

---
+++ catalog/server.go
@@ -0,0 +1,31 @@
+package catalog
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/usama1031/go-grpc-graphql-microservice/account/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
+	pb.UnimplementedAccountServiceServer
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+
+	serv := grpc.NewServer()
+	pb.RegisterAccountServiceServer(serv, &grpcServer{
+		UnimplementedAccountServiceServer: pb.UnimplementedAccountServiceServer{},
+		service:                           s,
+	})
+
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
